69d90c8a248b16419eb5322620afa2a583c8b201
Lucas Blotta <lucas@blotta.info>
2020-09-08 23:57:07 -0300

    Maki rotacionando

--- js/main.js
+++ js/main.js
@@ -3,35 +3,46 @@ let game;
 
 window.onload = function() {
 
-    game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamewindow', { preload: preload, create: create, update: update });
+    game = new Phaser.Game(1024, 576, Phaser.AUTO, 'gamewindow', { preload: preload, create: create, update: update });
 };
 
 /** @type {Phaser.Sprite} */
-let player;
+let maki_obj;
+
+/** @type {boolean} */
+let maki_estaRotacionando = false;
 
 /** @type {Phaser.Group} */
 let platforms;
 
-/** @type {Phaser.Group} */
-let cubes;
-
 function preload () {
 
-    let images = [];
+    let images = [
+        "maki_top"
+    ];
 
     for (let img of images) {
-        game.load.image("spr_" + img, "assets/" + img + ".png");
+        game.load.image("spr_" + img, "assets/images/" + img + ".png");
     }
 }
 
 function create () {
+    maki_obj = game.add.sprite(game.world.width / 2, game.world.height / 2, "spr_maki_top");
+    maki_obj.anchor.set(0.5);
+    maki_obj.scale.set(0.5);
+    maki_estaRotacionando = false;
 
 }
 
 function update() {
-
+    if (!maki_estaRotacionando) {
+        addRandomTween();
+    }
 }
 
-function collectCube(player, cube) {
-    cube.kill();
+function addRandomTween() {
+    game.add.tween(maki_obj)
+        .to({ angle: -180 + Math.random() * 360}, 1000 + Math.random() * 3000, Phaser.Easing.Linear.None, true)
+        .onComplete.add(() => maki_estaRotacionando = false);
+    maki_estaRotacionando = true;
 }
